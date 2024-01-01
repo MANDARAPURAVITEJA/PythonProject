19e2490b8c9ce54c10137fa032b594a2addf3c10
MANDARAPURAVITEJA <raviteja.vvss99@gmail.com>
2024-01-01 17:57:03 +0530

    Updated Commit

--- app.py
+++ app.py
@@ -1,28 +1,60 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_bcrypt import Bcrypt
+from flask import Flask, render_template, request, redirect, url_for, flash,jsonify
+import SQL_operations as sql_obj
+import pymysql
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
-bcrypt = Bcrypt()
-
-# Dummy user data (replace with a database in a real application)
-users = {'john': {'password': bcrypt.generate_password_hash('pass123').decode('utf-8')}}
 
 @app.route('/')
 def home():
     return render_template('login.html')
+
+@app.route('/homepage', methods=['GET'])
+def homepage():
+    row_data=sql_obj.homepage_data()
+
+    return render_template('homepage.html',data=row_data)
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
 
-    if username in users and bcrypt.check_password_hash(users[username]['password'], password):
+    if sql_obj.retrieve_data(username,password):
         flash('Login successful!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('homepage'))
     else:
         flash('Invalid username or password. Please try again.', 'danger')
-        return redirect(url_for('home'))
+        return redirect(url_for('homepage'))
+
+@app.route('/newuser',methods=['POST','GET']) #
+def index():
+    if request.method == 'POST':
+        try:
+            FIRST_NAME = request.form['FIRST_NAME']
+            LAST_NAME = request.form['LAST_NAME']
+            EMAILID = request.form['EMAILID']
+            PASSWORD = request.form['PASSWORD']
+            SECURITY_QUESTION = request.form['SECURITY_QUESTION']
+
+            student_info = sql_obj.Insert_Data(first_name=FIRST_NAME,
+                                              last_name=LAST_NAME,
+                                              emailid=EMAILID,
+                                              password=PASSWORD,
+                                              security_question=SECURITY_QUESTION
+                                        )
+            context = "Student details Submitted Successfully"
+            return render_template('index.html', context="New User Created Successfully")
+
+        except Exception as e:
+            print('The Exception message is: ',e)
+            context="Some issue with the code, details not saved. Contact IT Support"
+            return render_template('index.html',context=context)
+
+    else:
+        return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
